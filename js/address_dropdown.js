f8f14dd9cbaf697381e3742dd9cfabf7f8462981
mohanagavenkat <mohanagavenkat@gmail.com>
2019-01-11 10:18:54 +0530

    removed id from value of option and instead using data attributes to store id

--- js/address_dropdown.js
+++ js/address_dropdown.js
@@ -9,28 +9,38 @@ const pincode = document.getElementById("pincode");
 function addOptions() {
     var jsonArray = countries;
     let countryOptionsHTML = `<option value="">Select Country</option>`;
-    countryOptionsHTML += jsonArray.map(country => `<option value="${country.name}--${country.id}" >${country.name}</option>`).join("");
+    countryOptionsHTML += jsonArray.map(country => `<option data-id="${country.id}" value="${country.name}" >${country.name}</option>`).join("");
     countrySelect.innerHTML = countryOptionsHTML;
 }
 
-countrySelect.addEventListener("change", function () {
-    const [countryName, countryId] = countrySelect.value.split("--");
+countrySelect.addEventListener("change",getStates);
+
+function getStates(){
+    // const [countryName, countryId] = countrySelect.value.split("--");
+    const selectedCountry = countrySelect.options[countrySelect.selectedIndex];
+    const countryName = selectedCountry.value;
+    const countryId = selectedCountry.dataset.id;
     console.log(countryName, countryId);
     citySelect.innerHTML = `<option value="">Select City</option>`;
     pincode.value = "";
     let stateOptionsHTML = `<option value="">Select State</option>`;
-    stateOptionsHTML += states.filter(state => state.country_id === countryId).map(state => `<option value="${state.name}--${state.id}" >${state.name}</option>`).join("");
+    stateOptionsHTML += states.filter(state => state.country_id === countryId).map(state => `<option data-id="${state.id}" value="${state.name}" >${state.name}</option>`).join("");
     stateSelect.innerHTML = stateOptionsHTML;
-});
+}
 
-stateSelect.addEventListener("change", function () {
-    const [stateName, stateId] = stateSelect.value.split("--");
+stateSelect.addEventListener("change", getCities);
+
+function getCities(){
+    // const [stateName, stateId] = stateSelect.value.split("--");
+    const selectedState = stateSelect.options[stateSelect.selectedIndex];
+    const stateName = selectedState.value;
+    const stateId = selectedState.dataset.id;
     console.log(stateName, stateId);
     pincode.value = "";
     let cityOptionsHTML = `<option value="">Select City</option>`;
-    cityOptionsHTML += cities.filter(city => city.state_id === stateId).map(city => `<option value="${city.name}--${city.id}" >${city.name}</option>`).join("");
+    cityOptionsHTML += cities.filter(city => city.state_id === stateId).map(city => `<option value="${city.name}" >${city.name}</option>`).join("");
     citySelect.innerHTML = cityOptionsHTML;
-})
+}
 
 document.addEventListener("DOMContentLoaded",()=>{
     addOptions();
